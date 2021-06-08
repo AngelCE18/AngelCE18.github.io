e25c006e3e826a872200d7ce693cbff955ebb648
AngelCE18 <angel.eggerson@einsteincharterschools.org>
2021-06-08 19:54:53 +0000

    saving walker project

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,13 +10,23 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
-  // Game Item Objects
+  var KEY = {
+      "LEFT": 37,
+      "UP": 38,
+      "RIGHT": 39,
+      "DOWN": 40,
+  }
 
+  // Game Item Objects
+  var axisX = 0;
+  var axisY = 0;
+  var xSpeed = 0;
+  var ySpeed = 0;
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on("keyup", handleKeyUp);                               // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,17 +37,50 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    repositionGameItem();
+    redrawGameItem();
   }
   
   /* 
   Called in response to events.
   */
   function handleKeyDown(event) {
-      console.log("keydown");
+    if (event.which === KEY.LEFT){
+      xSpeed = -5;
+      console.log("left pressed");
+    }
+    else if (event.which === KEY.UP){
+      ySpeed = -5;
+      console.log("up pressed");
+    }
+    else if (event.which === KEY.RIGHT){
+      xSpeed = 5;
+      console.log("right pressed");
+    }
+    else {
+      ySpeed = 5;
+      console.log("down pressed");
+    }
   }
 
+  function handleKeyUp(event) {
+    if (event.which === KEY.LEFT){
+      xSpeed = 0;
+      console.log("left released");
+    }
+    else if (event.which === KEY.UP){
+      ySpeed = 0;
+      console.log("up released");
+    }
+    else if (event.which === KEY.RIGHT){
+      xSpeed = 0;
+      console.log("right released");
+    }
+    else {
+      ySpeed = 0;
+      console.log("down released");
+    }
+  }  
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -50,5 +93,20 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-  
+
+  function repositionGameItem(){
+    // moves the box along the x axis
+    axisX += xSpeed;
+    
+    // moves the box along the y axis
+    axisY += ySpeed;
+  }
+
+  function redrawGameItem(){
+    // draws the box in a new location and positions it away from the left
+    $("#gameItem").css("left", axisX);
+
+    // draws the box in a new location and positions it away from the top
+    $("#gameItem").css("top", axisY);
+  }
 }
